31b7801a60c376e58ed06ca576cbd05f25d8be6d
lxl2015 <1010361011@qq.com>
2018-01-02 16:02:01 +0800

    1

---
+++ include/lxl_cfg.h
@@ -0,0 +1,31 @@
+#ifndef LXL_CFG_H
+
+#define LXL_CFG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stting.h>
+
+
+#ifndef YES
+#define YES 1
+#endif
+
+#ifndef NO
+#define NO 0
+#endif
+
+
+#ifndef SUCCEED
+#define SUCCEED 0
+#endif
+
+#ifndef FAIL
+#define FAIL -1
+#endif
+
+
+
+
+#endif
---
+++ src/lxl_cfg.c
@@ -0,0 +1,11 @@
+/**
+ * @File      lxl_cfg.c
+ * @Function  
+ * @Author    LXL ,1010361011@qq.com
+ * @version   0.1
+ * @Date      2018-01-02
+ */
+
+
+#include "lxl_cfg.h"
+
